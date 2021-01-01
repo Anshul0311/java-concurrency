43bece857839a55bdc1504d3fc4e27105cfb50b7
anshulkapoor <anshul.kapoor@africa.airtel.com>
2021-01-01 15:36:32 +0530

    concurrent threads demo

---
+++ src/main/java/com/skillsoft/concurrency/service/ConcurrentThreads.java
@@ -0,0 +1,26 @@
+package com.skillsoft.concurrency.service;
+
+public class ConcurrentThreads implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 5; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " says: " + i);
+        }
+    }
+
+    public void concurrentDemo() throws InterruptedException {
+        Thread t1 = new Thread(new ConcurrentThreads());
+        Thread t2 = new Thread(new ConcurrentThreads());
+        t1.start();
+        t2.start();
+
+        //t1.start() won't work
+        //t1.run() will work
+    }
+}
