1cfaed348085ec60e61628f7ba842ba0aa683a67
anshulkapoor <anshul.kapoor@africa.airtel.com>
2021-01-01 14:24:42 +0530

    runnable demo

---
+++ src/main/java/com/skillsoft/concurrency/service/MyThreadRunnable.java
@@ -0,0 +1,18 @@
+package com.skillsoft.concurrency.service;
+
+public class MyThreadRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("This has been executed by a thread");
+    }
+
+    public void runnableDemo() {
+        Thread myThread = new Thread(new MyThreadRunnable());
+        myThread.start();
+
+        System.out.println("myThread instanceof Runnable ? " + (myThread instanceof Runnable));
+        System.out.println("myThread instanceof Thread ? " + (myThread instanceof Thread));
+        System.out.println("myThread instanceof Object ? " + (myThread instanceof Object));
+    }
+}
