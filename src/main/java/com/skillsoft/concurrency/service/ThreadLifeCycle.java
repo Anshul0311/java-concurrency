b673662888d4e462da391269a8fc81ac58123468
anshulkapoor <anshul.kapoor@africa.airtel.com>
2021-01-02 13:15:51 +0530

    thread lifecycle

---
+++ src/main/java/com/skillsoft/concurrency/service/ThreadLifeCycle.java
@@ -0,0 +1,46 @@
+package com.skillsoft.concurrency.service;
+
+public class ThreadLifeCycle {
+
+    //There will be a single instance of Walk class for the entire thread lifecycle, since its a static class
+    public static class Walk implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i < 5; i++) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("I am walking..");
+            }
+        }
+    }
+
+    public static class ChewGum implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i < 5; i++) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("I am chewing gum..");
+            }
+        }
+    }
+
+    public void performTasks() {
+        Thread walkThread = new Thread(new Walk());
+        Thread chewThread = new Thread(new ChewGum());
+        try {
+            walkThread.start();
+            walkThread.join();
+            chewThread.start();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
